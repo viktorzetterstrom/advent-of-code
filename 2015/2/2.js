f90a1127526704c0209eb3701138a5f69ab2de10
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-06-21 21:09:14 +0200

    2015 - day 2 - fix bug with ribbon length calculation

--- 2015/2/2.js
+++ 2015/2/2.js
@@ -3,15 +3,17 @@ const { readFile } = require("fs").promises;
 
 const calcSideArea = (s1, s2) => 2 * s1 * s2;
 const calcPaper = (w, h, l) => {
-  const sideOne = calcSideArea(w, h);
-  const sideTwo = calcSideArea(w, l);
-  const sideThree = calcSideArea(h, l);
-  return (
-    sideOne + sideTwo + sideThree + Math.min(sideOne, sideTwo, sideThree) / 2
-  );
+  const a1 = calcSideArea(w, h);
+  const a2 = calcSideArea(w, l);
+  const a3 = calcSideArea(h, l);
+  return a1 + a2 + a3 + Math.min(a1, a2, a3) / 2;
 };
 
-const calcRibbon = (w, h, l) => 2 * (R.min(w, h) + R.min(h, l)) + w * h * l;
+const calcRibbon = (w, h, l) => {
+  const diff = (a, b) => a - b;
+  const [s1, s2] = R.sort(diff, [w, h, l]);
+  return 2 * (s1 + s2) + w * h * l;
+};
 
 const run = async () => {
   const input = await readFile("./input.txt", { encoding: "utf-8" });
@@ -22,22 +24,21 @@ const run = async () => {
     R.map(R.map(Number))
   );
 
-  const part1Calculator = R.pipe(
+  const part1 = R.pipe(
     formatInput,
-    R.map((x) => calcPaper(...x)),
-    R.sum
+    R.map((x) => calcPaper(...x))
   );
 
-  const part2Calculator = R.pipe(
+  const part2 = R.pipe(
     formatInput,
     R.map((x) => calcRibbon(...x)),
     R.sum
   );
 
-  const part1 = part1Calculator(input);
-  const part2 = part2Calculator(input);
-  console.log("Part 1", part1);
-  console.log("Part 2", part2);
+  const p1 = part1(input);
+  const p2 = part2(input);
+  console.log("Part 1", p1);
+  console.log("Part 2", p2);
 };
 if (process.env.NODE_ENV !== "test") {
   run();
--- 2015/2/2.test.js
+++ 2015/2/2.test.js
@@ -11,6 +11,8 @@ describe("day 2", () => {
     it("calcs ribbon needed correctly", () => {
       expect(calcRibbon(2, 3, 4)).toEqual(34);
       expect(calcRibbon(1, 1, 10)).toEqual(14);
+      expect(calcRibbon(8, 23, 6)).toEqual(1132);
+      expect(calcRibbon(25, 2, 25)).toEqual(1304);
     });
   });
 });
