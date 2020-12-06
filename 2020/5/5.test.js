1cee2f7107f2a3e0336f88a5ab6b6132a0eb712b
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-12-06 10:22:58 +0100

    fix test comment, remove unused variable

--- 2020/5/5.test.js
+++ 2020/5/5.test.js
@@ -1,9 +1,9 @@
-const { loadInput, calculateSeatId, findHighestSeatId } = require("./5");
+const { loadInput, findHighestSeatId } = require("./5");
 
 const testInput = loadInput("test-input.txt");
 const highestId = 820;
 
-describe("day 4 part 1", () => {
+describe("day 5 part 1", () => {
   it("finds highest seatid", () => {
     expect(findHighestSeatId(testInput)).toBe(highestId);
   });
